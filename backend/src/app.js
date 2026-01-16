ab92cad31654c5e025cdca5f340dd7bd8e4d2791
hrshiti <hritik45raghuwanshi@gmail.com>
2026-01-16 19:22:06 +0530

    new changes in encv

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     process.env.FRONTEND_URL,
-    'https://appzeto2-1.vercel.app'
+    'https://appzeto.com'
 ].filter(Boolean);
 
 app.use(cors({
